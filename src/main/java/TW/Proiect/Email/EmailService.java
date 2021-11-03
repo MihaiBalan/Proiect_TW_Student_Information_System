ba6fb4bd720a832bf95af61f23fac4fdf649d328
MihaiBalan <mihai.balan00@e-uvt.ro>
2021-11-03 13:23:23 +0200

    Now it sends the email

--- src/main/java/TW/Proiect/Email/EmailService.java
+++ src/main/java/TW/Proiect/Email/EmailService.java
@@ -1,22 +1,14 @@
 package TW.Proiect.Email;
 
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
-import javax.mail.MessagingException;
 
 @Service
 @AllArgsConstructor
 public class EmailService implements EmailSender{
-
-    private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
 
     private final JavaMailSender mailSender;
 
